b76509c062c63900f4f78d02a0ea6c2d3d0dfb98
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-11-23 19:40:11 -0300

    Add calculator style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
 import './App.css';
+import React from 'react';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      Just a text
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
