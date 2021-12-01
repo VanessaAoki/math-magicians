741520e8fda70a366666da144502202abf657e3b
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-01 01:40:05 -0300

    Add the pages styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomePage = () => (
   <div className="homeContainer">
-    <h2>Welcome to our page</h2>
+    <h2>Welcome to our page!</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia itaque harum, corporis atque est modi totam similique asperiores. Repellat, aut?</p>
     <p>Cupiditate minima fugiat consequuntur dolor porro dolorem non! Asperiores officia nam delectus, temporibus enim nemo deleniti quisquam explicabo unde sit.</p>
   </div>
