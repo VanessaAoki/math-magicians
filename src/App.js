5015d7caf9e06feaac4c26a33a4211fbee65f296
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-01 08:06:40 -0300

    Add redirect to not have white pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from 'react-router-dom';
 import Header from './components/Header';
 import QuotePage from './pages/QuotePage';
@@ -18,6 +19,7 @@ export default function App() {
         <Header />
         <div className="body-container">
           <Switch>
+            <Redirect exact from="/" to="/home" />
             <Route path="/quote" component={QuotePage} />
             <Route path="/calculator" component={CalculatorPage} />
             <Route path="/home" component={HomePage} />
